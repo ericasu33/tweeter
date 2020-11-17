9c38c5c8f0cf7a969d709ceafe69dd6f863ef8e9
Erica <ericasu33@gmail.com>
2020-11-17 18:54:31 +0000

    update textarea and button design

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
   $("#tweet-text").on("input", function() {
     const countChar = $(this).val().length;
-    const remainingChar = 1 - countChar;
+    const remainingChar = 140 - countChar;
 
     let counter = $(this).parent().find("output");
     // console.log(this.form[2]);
     if (remainingChar < 0) {
-      counter.css({"color": "#FF8AFD"});
+      // console.log($(this).val());
+      counter.addClass("count-negative");
+      $(this).css("color", "#FF8AFD");
+    } else {
+      counter.removeClass("count-negative");
+      $(this).css("color", "#FF9B71");
     }
+    
     counter.html(remainingChar);
   });
+
 
 
 
