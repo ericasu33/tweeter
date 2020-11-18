8559e9bb08d4973823876760a1abdc0fcbf6b948
Erica <ericasu33@gmail.com>
2020-11-18 07:39:34 +0000

    Use ajax to send tweet data to server, and change tweet display from -old to new-  to -new to old-

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,34 +8,10 @@
 /*      Tweets      */
 /* ---------------- */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-
 $(document).ready(function() {
-
+  /* ---------------- */
+  /*   Tweet Display  */
+  /* ---------------- */
   const getTimestamp = function(tweetObj) {
     // Milliseconds in a day
     const oneDayByMs = 1000 * 60 * 60 * 24;
@@ -101,22 +77,40 @@ $(document).ready(function() {
     
     footer.append(timeStamp).append(icons);
 
+    // Appending article contents to <articel>
     const tweet = article.append(header).append(content).append(footer);
 
-    console.log(tweet);
     return tweet;
-
   };
 
   const renderTweets = function(tweets) {
     let tweet;
     for (const tweetObj of tweets) {
       tweet = createTweetElement(tweetObj);
-      $("#tweet-display").append(tweet);
+      $("#tweet-display").prepend(tweet);
     }
   };
 
-  renderTweets(data);
+  /* ---------------- */
+  /*   Tweet Posts    */
+  /* ---------------- */
+
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    
+    // Reset character counter to 140
+    $(".counter").html(140);
+
+    // New tweet content is sent to server
+    $.ajax("/tweets",
+      {
+        type: "POST",
+        data: $("form").serialize(),
+      });
+    
+
+  });
+
 });
 
 
