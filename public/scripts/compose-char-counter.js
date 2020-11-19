bcfa0d85bab7cd0b293a1bc8bcd7dd6d1530dfa6
Erica <ericasu33@gmail.com>
2020-11-19 09:07:01 +0000

    modify comment and made content, submit button fade if not meet char count validatoin.

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,22 +1,32 @@
-// Count characters inputted and reflect it on .counter
+/* ---------------- */
+/*  Tweet Textarea  */
+/* ---------------- */
 
 $(document).ready(function() {
+  // Set tweet button faded until input
+  $(".tweet-button").css("opacity", "0.4");
+  
+  // Count characters inputted and reflect it on .counter
   $("#tweet-text").on("input", function() {
     const noSpace = $.trim($(this).val());
     const countChar = noSpace.length;
     const remainingChar = 140 - countChar;
-
     const counter = $(this).parent().find("output");
 
     // change color once counter is negative
     if (remainingChar < 0) {
       counter.addClass("count-negative");
-      $(this).css("color", "#FF8AFD");
+      $(this).css("opacity", "0.4");
+      $(".tweet-button").css("opacity", "0.4");
+    } else if (remainingChar === 140) {
+      counter.removeClass("count-negative");
+      $(".tweet-button").css("opacity", "0.4");
     } else {
       counter.removeClass("count-negative");
-      $(this).css("color", "#FF9B71");
+      $(this).css("opacity", "");
+      $(".tweet-button").css("opacity", "");
     }
-    
+
     counter.html(remainingChar);
   });
 });
