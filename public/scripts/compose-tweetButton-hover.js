dae073d54c46b05e79645f3b648b9730781cc105
Erica <ericasu33@gmail.com>
2020-11-20 21:59:05 +0000

    add comments

--- public/scripts/compose-tweetButton-hover.js
+++ public/scripts/compose-tweetButton-hover.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
   $(".tweet-button").hover(function() {
     const counter = $(".counter").val();
     $(this).toggleClass("tweet-button-hover");
-    
+    // counter instanceof String
+    // Evaluator Note: Using coercion tactfully for simplicity and brevity.
     if (counter < 0 || counter == 140) {
       $(".tweet-button").removeClass("tweet-button-hover");
     }
