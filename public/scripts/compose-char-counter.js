6f6287f7d8722cc0bfae59b7b00a413304a0a59c
Erica <ericasu33@gmail.com>
2020-11-20 02:04:22 +0000

    refractor code -instead of .css used .addClass

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -4,7 +4,7 @@
 
 $(document).ready(function() {
   // Set tweet button faded until input
-  $(".tweet-button").css("opacity", "0.4");
+  $(".tweet-button").addClass("tweet-fade");
   
   // Count characters inputted and reflect it on .counter
   $("#tweet-text").on("input", function() {
@@ -16,15 +16,15 @@ $(document).ready(function() {
     // change color once counter is negative
     if (remainingChar < 0) {
       counter.addClass("count-negative");
-      $(this).css("opacity", "0.4");
-      $(".tweet-button").css("opacity", "0.4");
+      $(this).addClass("tweet-fade");
+      $(".tweet-button").addClass("tweet-fade");
     } else if (remainingChar === 140) {
       counter.removeClass("count-negative");
-      $(".tweet-button").css("opacity", "0.4");
+      $(".tweet-button").addClass("tweet-fade");
     } else {
       counter.removeClass("count-negative");
-      $(this).css("opacity", "");
-      $(".tweet-button").css("opacity", "");
+      $(this).removeClass("tweet-fade");
+      $(".tweet-button").removeClass("tweet-fade");
     }
 
     counter.html(remainingChar);
