1eaa35c0ad134510ea2663e95fed8e02fb41defc
Erica <ericasu33@gmail.com>
2020-11-20 07:16:14 +0000

    modify tweetbutton:hover to just regular css and use hover effect in js filed

---
+++ public/scripts/compose-tweetButton-hover.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $(".tweet-button").hover(function() {
+    const counter = $(".counter").val();
+    $(this).toggleClass("tweet-button-hover");
+    
+    if (counter < 0 || counter == 140) {
+      $(".tweet-button").removeClass("tweet-button-hover");
+    }
+  });
+});
