470acaecb3d145bf27f1aa3de805f34f182d6e56
Erica <ericasu33@gmail.com>
2020-11-22 19:10:06 +0000

    instead of using coercion, made number into string

--- public/scripts/compose-tweetButton-hover.js
+++ public/scripts/compose-tweetButton-hover.js
@@ -3,9 +3,7 @@ $(document).ready(function() {
   $(".tweet-button").hover(function() {
     const counter = $(".counter").val();
     $(this).toggleClass("tweet-button-hover");
-    // counter instanceof String
-    // Evaluator Note: Using coercion tactfully for simplicity and brevity.
-    if (counter < 0 || counter == 140) {
+    if (counter < 0 || counter === "140") {
       $(".tweet-button").removeClass("tweet-button-hover");
     }
   });
