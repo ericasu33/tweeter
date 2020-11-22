7107ed2645025fcb06fc6575f3c9fe638bb71b11
Erica <ericasu33@gmail.com>
2020-11-22 20:55:34 +0000

    modify nav bar button and its behaviour

--- public/scripts/compose-button-navbar.js
+++ public/scripts/compose-button-navbar.js
@@ -4,7 +4,15 @@
 
 $(document).ready(function() {
   $(".nav-new-tweet").on("click", function() {
+
+    setTimeout(function() {
+      $(".nav-view-tweet").toggle();
+      $(".nav-write-tweet").toggle();
+      $("#tweet-text").focus();
+    }, 500);
+    
     $("#tweet-input").toggle(1000);
-    $("#tweet-text").focus();
+    $(window).scrollTop(0);
   });
+
 });
--- public/scripts/compose-scroll-to-top.js
+++ public/scripts/compose-scroll-to-top.js
@@ -7,10 +7,8 @@ $(document).ready(function() {
   $(window).on("scroll", function() {
     if ($(window).scrollTop() > 20) {
       $("#scroll-to-top-button").show(700);
-      $(".nav-new-tweet").fadeOut(1000);
     } else {
       $("#scroll-to-top-button").hide(1000);
-      $(".nav-new-tweet").fadeIn(1000);
     }
   });
 
